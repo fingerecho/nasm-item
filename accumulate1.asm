c332c1e66d12b797456cdefffe6788f8e35d4831
方克铖 <18779875304@163.com>
2024-04-17 21:25:29 +0800

    Add files via upload

--- accumulate1.asm
+++ accumulate1.asm
@@ -1,6 +1,10 @@
 ;从1-X,计算从1+...+x的和
 ; x(1,1000)
 ; compile:nasm -f elf32 accumulate1.asm -o accumulate.o && gcc -m32 accumulate.o && ./a.out
+; (1+x)*x/2 = 2,147,483,647  ( 2^31 - 1)
+; x^2 + x - 4,294,967,294 = 0;
+; 得出 x = 65536
+; 当 x > 65536 时溢出
 
 extern printf
 extern exit
@@ -29,7 +33,7 @@ main:
 		push msg
 		call printf
 		
-	cmp dword[x],999
+	cmp dword[x],65538
 	jle @lp0
 
 	push 1
